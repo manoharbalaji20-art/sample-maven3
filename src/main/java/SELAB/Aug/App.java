fe6eaec5e93951ccf62a9b77b2bbb9e752db2788
Manohar Balaji <manoharbalaji20@gmail.com>
2026-09-07 05:09:53 +0530

    Initial Commit

---
+++ src/main/java/SELAB/Aug/App.java
@@ -0,0 +1,13 @@
+package SELAB.Aug;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
